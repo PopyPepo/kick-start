c989747f0a72a4a25ee9696746bc77c2c0f36d32
Kanaphol <kanaphol@sut.ac.th>
2024-05-31 14:11:04 +0700

    Delete Assets

--- app/admin.php
+++ app/admin.php
@@ -15,30 +15,9 @@ $ACTION = isset($rount[1]) ? $rount[1] : ($rount[0]=='layout' ? 'main' : 'list')
 	<!-- <base href="/"> -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- <meta name="description" content="Geeks is a fully responsive and yet modern premium bootstrap 5 template & snippets. Geek is feature-rich components and beautifully designed pages that help you create the best possible website and web application projects. Bootstrap Snippet " />
-	<meta name="keywords" content="Geeks UI, bootstrap, bootstrap 5, Course, Sass, landing, Marketing, admin themes, bootstrap admin, bootstrap dashboard, ui kit, web app, dash ui, admin dashboard, HTML5, multipurpose" /> -->
 
 	<!-- Favicon icon-->
-	<link rel="shortcut icon" type="image/x-icon" href="<?php echo $ASSETS_URL; ?>assets/img/favicon.jpg">
-
-
-	<!-- Libs CSS -->
-	<link href="<?php echo $ASSETS_URL; ?>assets/fonts/feather/feather.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/dragula/dist/dragula.min.css" rel="stylesheet" />
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/dropzone/dist/dropzone.css" rel="stylesheet" />
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/magnific-popup/dist/magnific-popup.css" rel="stylesheet" />
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/@yaireo/tagify/dist/tagify.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/tiny-slider/dist/tiny-slider.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/tippy.js/dist/tippy.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/prismjs/themes/prism-okaidia.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/simplebar/dist/simplebar.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/nouislider/dist/nouislider.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
-	<link href="<?php echo $ASSETS_URL; ?>assets/libs/file-upload-with-preview/dist/file-upload-with-preview.min.css" rel="stylesheet">
+	<!-- <link rel="shortcut icon" type="image/x-icon" href="<?php // echo $ASSETS_URL; ?>assets/img/favicon.jpg"> -->
 
 
 	<script type="text/javascript">
@@ -47,7 +26,7 @@ $ACTION = isset($rount[1]) ? $rount[1] : ($rount[0]=='layout' ? 'main' : 'list')
 		var LANG = '<?php echo $LANG; ?>';
 		var LINK = '<?php echo $LINK_URL; ?>';
 	</script>
-	<script src="<?php echo $ASSETS_URL; ?>assets/libs/jquery/dist/jquery.min.js"></script>
+
 	<script src="<?php echo $ASSETS_URL; ?>node_modules/angular/angular.min.js"></script>
 	<script src="<?php echo $ASSETS_URL; ?>node_modules/angular-sanitize/angular-sanitize.min.js"></script>
 	<script src="<?php echo $ASSETS_URL; ?>node_modules/angular-file-upload/dist/angular-file-upload.min.js"></script>
@@ -63,8 +42,6 @@ $ACTION = isset($rount[1]) ? $rount[1] : ($rount[0]=='layout' ? 'main' : 'list')
 	<link rel="stylesheet" href="<?php echo $ASSETS_URL; ?>node_modules/ng-notify/dist/ng-notify.min.css" type="text/css">
 	<!-- Theme CSS -->
 	<link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-	<link rel="stylesheet" href="<?php echo $ASSETS_URL; ?>assets/css/theme.min.css">
-	<link rel="stylesheet" href="<?php echo $ASSETS_URL; ?>assets/css/custom.css">
 
 	<title> ADMINISTRATOR </title>
 </head>
@@ -106,59 +83,11 @@ $ACTION = isset($rount[1]) ? $rount[1] : ($rount[0]=='layout' ? 'main' : 'list')
 
 
 
-	<!-- Scripts -->
-	<!-- Libs JS -->
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/odometer/odometer.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/flatpickr/dist/flatpickr.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/inputmask/dist/jquery.inputmask.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/quill/dist/quill.min.js"></script>
-<!-- <script src="<?php // echo $ASSETS_URL; ?>assets/libs/file-upload-with-preview/dist/file-upload-with-preview.iife.js"></script> -->
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/dragula/dist/dragula.min.js"></script>
-<!-- <script src="<?php // echo $ASSETS_URL; ?>assets/libs/bs-stepper/dist/js/bs-stepper.min.js"></script> -->
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/dropzone/dist/min/dropzone.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/jQuery.print/jQuery.print.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/prismjs/prism.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/prismjs/components/prism-scss.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/@yaireo/tagify/dist/tagify.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/tiny-slider/dist/min/tiny-slider.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/@popperjs/core/dist/umd/popper.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/tippy.js/dist/tippy-bundle.umd.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/typed.js/lib/typed.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/jsvectormap/dist/js/jsvectormap.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/jsvectormap/dist/maps/world.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/fullcalendar/main.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/@lottiefiles/lottie-player/dist/lottie-player.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/simplebar/dist/simplebar.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/nouislider/dist/nouislider.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/wnumb/wNumb.min.js"></script>
-<script src="<?php echo $ASSETS_URL; ?>assets/libs/glightbox/dist/js/glightbox.min.js"></script>
-
-
 <!-- CDN File for moment -->
 <script src='https://cdn.jsdelivr.net/npm/moment@2.29.1/min/moment.min.js'></script>
-<!-- Theme JS -->
-<script src="<?php echo $ASSETS_URL; ?>assets/js/theme.min.js"></script>
+
 
 <script>
-$(document).ready(function() {
-	if ($('#editTeacherModal').length && $('#app_teacherController').length) {
-		const myModalEl = document.getElementById('editTeacherModal')
-		myModalEl.addEventListener('hidden.bs.modal', event => {
-			angular.element(document.getElementById('app_teacherController')).scope().initFrom();
-		});
-	}
-});
-
 	function openModal(idModal){
 		$('#'+idModal).modal('show');
 	}
--- app/home.php
+++ app/home.php
@@ -14,30 +14,13 @@ $actual_link = urlencode($actual_url.$actual_page);
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="shortcut icon" type="image/x-icon" href="./assets/img/favicon.jpg">
 	
 	<meta property="og:url" content="<?php echo $actual_link; ?>" />
-	<meta property="og:image" content="<?php echo $actual_url.$FORDER; ?>/assets/img/header.jpg" />
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content="FMS LifeLong Learning" />
 	<!-- <meta property="og:description" content="DESCRIPTION OF PAGE" /> -->
 
-	<!-- Libs CSS -->
-	<link href="./assets/fonts/feather/feather.css" rel="stylesheet">
-	<link href="./assets/libs/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
-	<link href="./assets/libs/dragula/dist/dragula.min.css" rel="stylesheet" />
-	<link href="./assets/libs/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet" />
-	<link href="./assets/libs/dropzone/dist/dropzone.css" rel="stylesheet" />
-	<link href="./assets/libs/magnific-popup/dist/magnific-popup.css" rel="stylesheet" />
-	<link href="./assets/libs/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-	<link href="./assets/libs/@yaireo/tagify/dist/tagify.css" rel="stylesheet">
-	<link href="./assets/libs/tiny-slider/dist/tiny-slider.css" rel="stylesheet">
-	<link href="./assets/libs/tippy.js/dist/tippy.css" rel="stylesheet">
-	<link href="./assets/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
-	<link href="./assets/libs/prismjs/themes/prism-okaidia.min.css" rel="stylesheet">
-	<link href="./assets/libs/simplebar/dist/simplebar.min.css" rel="stylesheet">
-	<link href="./assets/libs/nouislider/dist/nouislider.min.css" rel="stylesheet">
-	<link href="./assets/libs/glightbox/dist/css/glightbox.min.css" rel="stylesheet">
+
 
 
 	<script type="text/javascript">
@@ -46,7 +29,6 @@ $actual_link = urlencode($actual_url.$actual_page);
 		var LANG = '<?php echo $LANG; ?>';
 		var LINK = '<?php echo $LINK_URL; ?>';
 	</script>
-	<script src="./assets/libs/jquery/dist/jquery.min.js"></script>
 	<script src="./node_modules/angular/angular.min.js"></script>
 	<script src="./node_modules/angular-file-upload/dist/angular-file-upload.min.js"></script>
 	<script src="./node_modules/angular-utils-pagination/dirPagination.js"></script>
@@ -63,8 +45,7 @@ $actual_link = urlencode($actual_url.$actual_page);
 	<link rel="stylesheet" href="./node_modules/ng-notify/dist/ng-notify.min.css" type="text/css">
 	<!-- Theme CSS -->
 	<link rel="stylesheet" href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-	<link rel="stylesheet" href="./assets/css/theme.min.css">
-	<link rel="stylesheet" href="./assets/css/custom.css">
+
 
 	<title> KICK-Start </title>
 </head>
@@ -116,51 +97,11 @@ $actual_link = urlencode($actual_url.$actual_page);
 	<?php include('app/layout/component/footer.php'); ?>
 
 
-	<!-- Scripts -->
-	<!-- Libs JS -->
-
-<script src="./assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<script src="./assets/libs/odometer/odometer.min.js"></script>
-<script src="./assets/libs/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-<script src="./assets/libs/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-<script src="./assets/libs/flatpickr/dist/flatpickr.min.js"></script>
-<script src="./assets/libs/inputmask/dist/jquery.inputmask.min.js"></script>
-<script src="./assets/libs/apexcharts/dist/apexcharts.min.js"></script>
-<script src="./assets/libs/quill/dist/quill.min.js"></script>
-<script src="./assets/libs/file-upload-with-preview/dist/file-upload-with-preview.iife.js"></script>
-<script src="./assets/libs/dragula/dist/dragula.min.js"></script>
-<script src="./assets/libs/bs-stepper/dist/js/bs-stepper.min.js"></script>
-<script src="./assets/libs/dropzone/dist/min/dropzone.min.js"></script>
-<script src="./assets/libs/jQuery.print/jQuery.print.js"></script>
-<script src="./assets/libs/prismjs/prism.js"></script>
-<script src="./assets/libs/prismjs/components/prism-scss.min.js"></script>
-<script src="./assets/libs/@yaireo/tagify/dist/tagify.min.js"></script>
-<script src="./assets/libs/tiny-slider/dist/min/tiny-slider.js"></script>
-<script src="./assets/libs/@popperjs/core/dist/umd/popper.min.js"></script>
-<script src="./assets/libs/tippy.js/dist/tippy-bundle.umd.min.js"></script>
-<script src="./assets/libs/typed.js/lib/typed.min.js"></script>
-<script src="./assets/libs/jsvectormap/dist/js/jsvectormap.min.js"></script>
-<script src="./assets/libs/jsvectormap/dist/maps/world.js"></script>
-<script src="./assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="./assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-<script src="./assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="./assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-<script src="./assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js"></script>
-<script src="./assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js"></script>
-<script src="./assets/libs/fullcalendar/main.min.js"></script>
-<script src="./assets/libs/@lottiefiles/lottie-player/dist/lottie-player.js"></script>
-<script src="./assets/libs/simplebar/dist/simplebar.min.js"></script>
-<script src="./assets/libs/nouislider/dist/nouislider.min.js"></script>
-<script src="./assets/libs/wnumb/wNumb.min.js"></script>
-<script src="./assets/libs/glightbox/dist/js/glightbox.min.js"></script>
 
 <!-- CDN File for moment -->
 <script src='https://cdn.jsdelivr.net/npm/moment@2.29.1/min/moment.min.js'></script>
 
-<!-- Theme JS -->
-<script src="./assets/js/theme.min.js"></script>
-<script src="./assets/js/vendors/tnsSlider.js"></script>
-<script src="./assets/js/vendors/tooltip.js"></script>
+
 
 
 <script type="text/javascript">
